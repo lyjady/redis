1bded4a9f96e193852446376fd8b2445276e5120
991296755@qq.com <lyj112233>
2019-08-15 14:17:09 +0800

    完善String指令

--- src/test/java/com/example/redis/TestRedisString.java
+++ src/test/java/com/example/redis/TestRedisString.java
@@ -4,11 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -88,5 +93,65 @@ public class TestRedisString {
         System.out.println("追加之前的长度: " + size);
         System.out.println("追加之后的长度: " + afterSize);
         System.out.println("追加之后的内容 " + string.get("address"));
+    }
+
+    @Test
+    public void testIncr() {
+        //测试incr key and incrby key increment and incrbyflaot key increment指令
+        ValueOperations<String, String> string = stringTemplate.opsForValue();
+        String originAge = string.get("age");
+        //根据第二个参数来指定增量,以执行不同的指令
+        string.increment("age", 2);
+        String afterAge = string.get("age");
+        System.out.println("之前的age: " + originAge);
+        System.out.println("之后的age: " + afterAge);
+    }
+
+    @Test
+    public void testDecr() {
+        //测试decr key and decrby key increment 指令
+        ValueOperations<String, String> string = stringTemplate.opsForValue();
+        String originAge = string.get("age");
+        //根据第二个参数来指定减量,以执行不同的指令
+        string.decrement("age", 4);
+        String afterAge = string.get("age");
+        System.out.println("之前的age: " + originAge);
+        System.out.println("之后的age: " + afterAge);
+    }
+
+    @Test
+    public void testMset() {
+        //测试mset key value...
+        ValueOperations<String, String> string = stringTemplate.opsForValue();
+        Map<String, String> keyValue = new HashMap<>();
+        keyValue.put("gender", "girl");
+        keyValue.put("hobby", "swim");
+        string.multiSet(keyValue);
+        System.out.println(string.get("gender"));
+        System.out.println(string.get("hobby"));
+    }
+
+    @Test
+    public void testMetnxAndMget() {
+        //测试 msetnx key value... and mget key...
+        ValueOperations<String, String> string = stringTemplate.opsForValue();
+        Map<String, String> keyValue = new HashMap<>();
+        //存在的key(原子操作,如果已经有键存在那个键将不被设置,那么其他的键也不会被设置)
+//        keyValue.put("name", "Jane");
+//        keyValue.put("password", "112233");
+        //不存在的key
+        keyValue.put("email", "Rose@hh.com");
+        keyValue.put("language", "China");
+        string.multiSetIfAbsent(keyValue);
+        //mget
+        List<String> keys = new ArrayList<>();
+        keys.add("name");
+        keys.add("password");
+        keys.add("email");
+        keys.add("language");
+        keys.add("gender");
+        keys.add("hobby");
+        List<String> values = string.multiGet(keys);
+        System.out.println(values);
     }
 }
